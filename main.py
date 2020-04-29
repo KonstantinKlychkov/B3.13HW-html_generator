763e5d5c9463f36066b12927ad1098464aff51e4
KonstantinKlychkov <klychkovks@gmail.com>
2020-04-29 17:21:34 +0300

    Описан class HTML. При создании экземпляра класса формируется шаблон HTML-кода, который записывается в строковую переменную или в файл *.html

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
 class Tag:
-    # ...
+    pass
+
 
 class HTML:
-    # ...
+    """Класс создаёт HTML-контейнер в котором хранится генерируемый HTML-код. 
+    Контейнер может быть строкой или файлом *.html"""
+
+    def __init__(self, output_file = False, file_name = 'index', file_path = ''):
+        self.output_file = output_file
+        self.code = ''
+        self.file_name = file_name
+        self.file_path = file_path
+
+        # Если оутпут в файл, то сразу же создадим файл c именем 'file_name.html' в директории 'file_path' или той же директории где модуль Питона и добавим в него:
+        """<!DOCTYPE html>
+            <html lang="en">
+            
+            </html>"""
+        if self.output_file == False:
+            # создадим элемент класса - строку с шаблоном HTML.
+            self.code = ('<!DOCTYPE html>\n'
+                            '<html lang="en">\n\n'
+                            '</html>')
+        elif self.output_file == True:
+            #создадим элемент класса - HTML- файл и запишем туда
+            with open(f'{self.file_path}{self.file_name}.html', 'w+') as file:
+                file.write('<!DOCTYPE html>\n'
+                            '<html lang="en">\n\n'
+                            '</html>')
+
+    def __str__(self):
+        if self.output_file == True:
+            with open(f'{self.file_path}{self.file_name}.html', 'r') as file:
+                self.code = file.read()
+        return  self.code      
+
+
 
 class TopLevelTag:
-    # ...
+    def __add__(self, other):
+        """Функция добавляет кусок html кода в переменную или в файл, в зависимости от аргумента 'output' элемента класса"""
+        # Если оутпут в переменную, то вставить кусок кода в конец перед тэгом </html>
+        # Если оутпут в файл, то вставить кусок кода в конец файла перед тэгом </html> записать и закрыть файл.
+        #if self.output_file == True:
+         #   with open(f'{self.file_name}.html', 'r') as file:
+          #      self.code = file.read()    pass
 
 
 if __name__ == "__main__":
-    with HTML(output=None) as doc:
+   doc = HTML(output_file = True, file_name = 'main', file_path = '/Users/a18301335/Python/Skill_Factory_FullStack/b2.14-hw-dummy-access/')
+   print(doc)
+   
+"""with HTML(output=None) as doc:
         with TopLevelTag("head") as head:
             with Tag("title") as title:
                 title.text = "hello"
@@ -31,4 +73,4 @@ if __name__ == "__main__":
 
                 body += div
 
-            doc += body
+            doc += body"""
